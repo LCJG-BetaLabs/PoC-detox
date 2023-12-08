08bdb4bd29df13696e34735c4b5ee967e9a31b9e
joechung-lcjg <joechung@lcjgroup.com>
2023-12-08 11:46:36 +0800

    fix:mock success test

--- TestDetox/e2e/starter.test.js
+++ TestDetox/e2e/starter.test.js
@@ -9,18 +9,18 @@ describe('Example', () => {
 
   it('should have welcome screen', async () => {
     // await expect(element(by.id('welcome'))).toBeVisible();
-    await expect(1).toBe(1);
+    expect(1).toBe(1);
   });
 
   it('should show hello screen after tap', async () => {
     // await element(by.id('hello_button')).tap();
     // await expect(element(by.text('Hello!!!'))).toBeVisible();
-    await expect(1).toBe(1);
+    expect(1).toBe(1);
   });
 
   it('should show world screen after tap', async () => {
     // await element(by.id('world_button')).tap();
     // await expect(element(by.text('World!!!'))).toBeVisible();
-    await expect(1).toBe(1);
+    expect(1).toBe(1);
   });
 });
